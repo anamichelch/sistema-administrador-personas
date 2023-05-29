4e69cfe157e44b77ba54d7f7e8966b69e278db61
anamichelch <ana.michelc13@gmail.com>
2023-05-29 09:51:09 -0500

    finished sorting by id

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -7,6 +7,8 @@ from personas.models import Persona
 # Create your views here.
 def bienvenido(request):
     no_personas = Persona.objects.count()
-    personas = Persona.objects.all()
+    #personas = Persona.objects.all()
+    personas = Persona.objects.order_by('id')
+
     return render(request, 'bienvenido.html',{'no_personas':no_personas,"personas":personas} )
 
